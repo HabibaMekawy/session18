131610e91545e67d9b473dce05dd50abb44cdc58
HabibaMekawy <habiba_sameh@icloud.com>
2021-06-21 21:33:05 +0200

    init commit

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+let  menuIcon = document.querySelector('.menu');
+let menu = document.querySelector('.header .menu');
+let icon=document.querySelector('links');
+
+
+menuIcon.addEventListener('click' , function(){
+    menu.style.display = 'block';
+    
+});
+/*
+closeMenu.addEventListener('click' , function(){
+    menu.style.display='none';
+}); */
+/*
+let extraImage = document.querySelectorAll('.extra');
+let showMore = document.querySelector('.our=work .show-more');
+let imageCount;*/
+
+
+
+
+
+icon.addEventListener('click' ,function(){
+    menuIcon.style.display='block';
+
+});
+
+
